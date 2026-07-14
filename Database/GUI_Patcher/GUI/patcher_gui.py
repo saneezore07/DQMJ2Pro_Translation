46388888568b1efa94977cff82c110dd74bb607e
anon <anon@anon.com>
2026-07-14 18:26:25 +1000

    Bump GUI version to v0.7.0

--- Database/GUI_Patcher/GUI/patcher_gui.py
+++ Database/GUI_Patcher/GUI/patcher_gui.py
@@ -28,7 +28,7 @@ def app_root():
     return Path(__file__).resolve().parents[3]
 
 ROOT = app_root()
-PATCHER_VERSION = "0.6.3"
+PATCHER_VERSION = "0.7.0"
 
 def open_url(url):
     if sys.platform.startswith("linux"):
